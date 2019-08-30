d79e95d592a372bed52623f8390d42c54530dd19
Cindy Anahy Beltran Esperano <cindy.beltran@coppel.com>
2019-08-30 15:10:33 -0600

    Se agregan comando de git

--- app/src/main/java/com/cedisropa/pruebaarquitecturabrandon2/api/APIPruebas.java
+++ app/src/main/java/com/cedisropa/pruebaarquitecturabrandon2/api/APIPruebas.java
@@ -3,8 +3,6 @@ package com.cedisropa.pruebaarquitecturabrandon2.api;
 import com.cedisropa.pruebaarquitecturabrandon2.entities.StructureResponse;
 import com.google.gson.JsonObject;
 
-import java.util.List;
-
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
